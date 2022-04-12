670826350e2728ec7980a0ce6a495261605cc50c
Panther254 <orwahanny@gmail.com>
2022-04-12 18:32:29 +0300

    Routes Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './Home.js'
+import SignIn from './SignIn.js'
+import SignUp from './SignUp.js'
+import Profile from './Profile.js'
+import About from './About.js'
+import Services from './Services.js'
+import Samples from './Samples.js'
+import Blog from './Blog.js'
+import Contact from './Contact.js'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/samples" element={<Samples />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact-us" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SignIn.css'
+
+const SignIn = () => {
+	return (
+		<div className="SignIn">
+			<h1>SignIn</h1>
+		</div>
+	)
+}
+
+export default SignIn
